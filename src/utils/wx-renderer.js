4478e08ba7763c213950e53756259b4fa0a07af6
cantonbolo <cantonbolo@gmail.com>
2024-08-22 18:13:07 +0800

    配置文件解耦

--- src/utils/wx-renderer.js
+++ src/utils/wx-renderer.js
@@ -8,6 +8,65 @@ marked.use(markedKatex({
   nonStandard: true,
 }))
 
+/*
+const descriptionList = {
+  name: 'descriptionList',
+  level: 'block',                                     // Is this a block-level or inline-level tokenizer?
+  start(src) { return src.match(/:[^:\n]/)?.index; }, // Hint to Marked.js to stop and check for a match
+  tokenizer(src, tokens) {
+    const rule = /^(?::[^:\n]+:[^:\n]*(?:\n|$))+/;    // Regex for the complete token, anchor to string start
+    const match = rule.exec(src);
+    if (match) {
+      const token = {                                 // Token to generate
+        type: 'descriptionList',                      // Should match "name" above
+        raw: match[0],                                // Text to consume from the source
+        text: match[0].trim(),                        // Additional custom properties
+        tokens: []                                    // Array where child inline tokens will be generated
+      };
+      this.lexer.inline(token.text, token.tokens);    // Queue this data to be processed for inline tokens
+      return token;
+    }
+  },
+  renderer(token) {
+    return `<dl>${this.parser.parseInline(token.tokens)}\n</dl>`; // parseInline to turn child tokens into HTML
+  }
+};
+
+const description = {
+  name: 'description',
+  level: 'inline',                                 // Is this a block-level or inline-level tokenizer?
+  start(src) { return src.match(/:/)?.index; },    // Hint to Marked.js to stop and check for a match
+  tokenizer(src, tokens) {
+    const rule = /^:([^:\n]+):([^:\n]*)(?:\n|$)/;  // Regex for the complete token, anchor to string start
+    const match = rule.exec(src);
+    if (match) {
+      return {                                         // Token to generate
+        type: 'description',                           // Should match "name" above
+        raw: match[0],                                 // Text to consume from the source
+        dt: this.lexer.inlineTokens(match[1].trim()),  // Additional custom properties, including
+        dd: this.lexer.inlineTokens(match[2].trim())   //   any further-nested inline tokens
+      };
+    }
+  },
+  renderer(token) {
+    return `\n<dt>${this.parser.parseInline(token.dt)}</dt><dd>${this.parser.parseInline(token.dd)}</dd>`;
+  },
+  childTokens: ['dt', 'dd'],                 // Any child tokens to be visited by walkTokens
+};
+
+function walkTokens(token) {                        // Post-processing on the completed token tree
+  if (token.type === 'strong') {
+    token.text += ' walked';
+    token.tokens = this.Lexer.lexInline(token.text)
+  }
+}
+*/
+// marked.use({ extensions: [descriptionList, description], walkTokens });
+
+console.log(marked.parse(`A Description List:\n`
++ `:   Topic 1   :  Description 1\n`
++ `: **Topic 2** : *Description 2*`))
+
 class WxRenderer {
   constructor(opts) {
     this.opts = opts
